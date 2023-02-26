7eaf514a64a506d8c1ed3850f3dfd8a1716a5082
Chris Hansen <chris.hansen1298@gmail.com>
2023-02-26 16:26:44 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_chanse99.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_chanse99.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private BookstoreContext context { get; set; }
+
+        public HomeController(BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = context.Books.ToList();
+
+            return View(blah);
+        }
+
+    }
+}
